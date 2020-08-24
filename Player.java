a7c8eb56c4d1ad8d8d8245a71b3ba4e5b4d8c0bf
Adriel Isaiah Amoguis <adriel_amoguis@dlsu.edu.ph>
2020-08-25 01:52:16 +0800

    Some progress
    Yes, there is an utter need to redesign the UML diagram to try and place all human interaction onto the main ThatsLife class.

--- Player.java
+++ Player.java
@@ -23,4 +23,66 @@ public class Player
         children = new ArrayList<Child>();
         space = null;
     }
+
+    public String getName()
+    {
+        return new String(this.playerName);
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void credit(double amount)
+    {
+        this.balance += amount;
+    }
+
+    public boolean debit(double amount)
+    {
+        if(this.balance < amount)
+            return false;
+        
+        this.balance -= amount;
+        return true;
+    }
+
+    public void setCareer(CareerCard career)
+    {
+        this.career = career;
+    }
+
+    public void setHouse(House house)
+    {
+        this.house = house;
+    }
+
+    public void setSpouse(boolean hasSpouse)
+    {
+        this.spouse = hasSpouse;
+    }
+
+    public void addChild(Child child)
+    {
+        this.children.add(child);
+    }
+
+    // MOVE
+
+    @Override
+    public String toString()
+    {
+        return (
+            "[" + this.playerName + "] Balance = " + String.valueOf(this.balance)
+        );
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj == null)
+            return false;
+        return this.playerName.equals(((Player) obj).getName());
+    }
 }
